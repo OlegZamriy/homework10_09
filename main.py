0628179ef80daef1668e1f94b0d8ede7dafc8b1b
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-09-25 21:49:48 +0300

    1

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
+
+suma = num1 + num2 + num3
+
+
+dobutok = num1 * num2 * num3
+
+print(f"Сума чисел: {suma}")
+print(f"Добуток чисел: {dobutok}")
